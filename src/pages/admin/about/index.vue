d7a43f29e4f002bf8e10bd27d30aac37d67d356d
heshame <atsh@gmail.com>
2024-09-16 09:58:55 +0300

    navigator.contacts

--- src/pages/admin/about/index.vue
+++ src/pages/admin/about/index.vue
@@ -1,14 +1,12 @@
 <template>
-  <v-container>
-    <ThemeSettings v-if="userStore.user" ref="dialogThemeForm" :detTheme="{ nameEn: 'about', nameAr: 'عني' }" @click="openDialogThemeForm" />
-    <!-- <SkillsForm ref="itemForm" @runItems="getItems" /> -->
-    <div style="white-space: nowrap" dir="rtl" v-if="items.length > 0" class="h"></div>
-    <p class="text-alert" v-else>لا توجد اي بيانات</p>
-    <div>
-      <v-text-field v-model="selectedContact" label="Selected Contact" readonly />
-      <v-btn @click="pickContact"> اختر جهة اتصال </v-btn>
-    </div>
-  </v-container>
+  <ThemeSettings v-if="userStore.user" ref="dialogThemeForm" :detTheme="{ nameEn: 'about', nameAr: 'عني' }" @click="openDialogThemeForm" />
+  <!-- <SkillsForm ref="itemForm" @runItems="getItems" /> -->
+  <div style="white-space: nowrap" dir="rtl" v-if="items.length > 0" class="h"></div>
+  <p class="text-alert" v-else>لا توجد اي بيانات</p>
+  <div>
+    <v-text-field type="number" v-model="selectedContactTel" :label="selectedContactNam ? selectedContactNam : 'ادخل رقم'" readonly />
+    <v-btn v-if="isContact" @click="pickContact"> اختر جهة اتصال </v-btn>
+  </div>
 </template>
 
 <script setup>
@@ -33,17 +31,26 @@ function openDialogThemeForm() {
     console.error('opendialogThemeForm is not a function');
   }
 }
-onMounted(() => {});
+const isContact = ref(null);
+onMounted(() => {
+  if (navigator.contacts) {
+    isContact.value = true;
+  }
+  console.log('navigator', navigator);
+  console.log('navigator.contacts', navigator.contacts);
+});
 
 //////////
 
-const selectedContact = ref('');
+const selectedContactTel = ref('');
+const selectedContactNam = ref('');
 
 async function pickContact() {
   try {
     const contacts = await navigator.contacts.select(['name', 'tel'], { multiple: false });
     if (contacts.length > 0) {
-      selectedContact.value = `${contacts[0].name[0]} - ${contacts[0].tel[0]}`;
+      selectedContactTel.value = contacts[0].tel[0];
+      selectedContactNam.value = contacts[0].name[0];
     }
   } catch (error) {
     console.error('حدث خطأ:', error);
